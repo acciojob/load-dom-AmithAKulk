303e945df1bd5fb971ae8c750b0048d96005939a
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-27 20:28:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 //your JS code here. If required.
-const c = document.getElementById("btn");
-c.addEventListener("click",() => {
-	document.write("DOM load success");
-});
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the body element
+    var body = document.body;
+
+    // Check if the body has any child nodes
+    if (body.hasChildNodes()) {
+      // If it has child nodes, remove them
+      while (body.firstChild) {
+        body.removeChild(body.firstChild);
+      }
+    }
+
+    // Create a text node with the content "DOM load success"
+    var textNode = document.createTextNode('DOM load success');
+
+    // Append the text node to the body
+    body.appendChild(textNode);
+  });
